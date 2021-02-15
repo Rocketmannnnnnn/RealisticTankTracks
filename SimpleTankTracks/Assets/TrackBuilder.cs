4eda7cc411f9bbaf9ceed1f304b51d44550965c0
Rocketmannnnnnn <jasper.bonefaas@gmail.com>
2021-02-15 10:42:06 +0100

    Added first test with Bezier curve

---
+++ SimpleTankTracks/Assets/TrackBuilder.cs
@@ -0,0 +1,28 @@
+﻿using PathCreation;
+using PathCreation.Examples;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackBuilder : MonoBehaviour
+{
+    public GameObject obj;
+    public PathCreator pathCreator;
+    public float speed = 0.2f;
+    public int trackAmount = 10;
+    List<GameObject> gameObjects = new List<GameObject>();
+
+    private void Start()
+    {
+        for (int i = 0; i < trackAmount; i++)
+        {
+            GameObject piece = Instantiate(obj);
+            gameObjects.Add(piece);
+            PathFollower flw = piece.AddComponent<PathFollower>();
+            flw.pathCreator = this.pathCreator;
+            flw.endOfPathInstruction = EndOfPathInstruction.Loop;
+            flw.speed = this.speed;
+            flw.distanceTravelled = (float)i / (float)trackAmount * pathCreator.path.length;
+        }
+    }
+}
